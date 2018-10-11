dbcfab76139c144a753c71e1fa7bea7accece1d5
Daniel Kapper <dkapper01@gmail.com>
2018-10-11 16:54:26 -0400

    Added awsome modal with input field to the add button

--- src/components/ButtonList/AddButtonToList.js
+++ src/components/ButtonList/AddButtonToList.js
@@ -25,12 +25,72 @@ class AddButtonToList extends React.Component {
         <div className="collapse" id="collapse">
           <div className="card card-body">
             <ListOfButtons buttonsText={this.state.buttons} />
-            <button type="button" className="" data-toggle="modal" data-target="#exampleModal">
-              <i
-                class="fa fa-plus-circle circle-add text-primany visible"
-                aria-hidden="true"
-              />
+
+            <button
+              type="button"
+              className="btn btn-primary invisible"
+              data-toggle="modal"
+              data-target="#exampleModal"
+              data-whatever="@getbootstrap"
+            >
+              <i class="fa fa-plus-circle circle-add visible" aria-hidden="true" />
             </button>
+
+            <div
+              className="modal fade"
+              id="exampleModal"
+              tabIndex="-1"
+              role="dialog"
+              aria-labelledby="exampleModalLabel"
+              aria-hidden="true"
+            >
+              <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title" id="exampleModalLabel">
+                      Add Button
+                    </h5>
+                    <button
+                      type="button"
+                      className="close"
+                      data-dismiss="modal"
+                      aria-label="Close"
+                    >
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div className="modal-body">
+                    <form>
+                      <div className="form-group">
+                        <label
+                          htmlFor="recipient-name"
+                          className="col-form-label"
+                        >
+                          Name:
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="recipient-name"
+                        />
+                      </div>
+                    </form>
+                  </div>
+                  <div className="modal-footer">
+                    <button
+                      type="button"
+                      className="btn btn-secondary"
+                      data-dismiss="modal"
+                    >
+                      Cancel
+                    </button>
+                    <button type="button" className="btn btn-primary">
+                      Add
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
 
             <AddBtnField handleAddButton={this.handleAddButton} />
           </div>
