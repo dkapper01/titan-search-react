cbddb0789257021ec3e37d7f4f509d5a3e6cc03a
Daniel Kapper <dkapper01@gmail.com>
2018-10-23 15:29:30 -0400

    cleaned up commits

--- src/components/actionButtons/ActionButtons.js
+++ src/components/actionButtons/ActionButtons.js
@@ -1,21 +1,5 @@
 import React from "react";
 
-// class ActionButtons extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
-
-//   render() {
-//     return (
-//       <div>
-//           <button>Go Back</button>
-//           <button onChange={this.props.handleDeleteAll}>Delete All</button>
-//           <button>Toggle</button>
-//       </div>
-//     )
-//   }
-// }
-console.log("working");
 
 const ActionButtons = props => {
 
