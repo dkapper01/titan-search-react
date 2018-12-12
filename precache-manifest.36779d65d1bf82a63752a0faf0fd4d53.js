20bcbd21e4eae20cd82a07b61bc59dffecea278c
Daniel Kapper <dkapper01@gmail.com>
2018-12-11 20:38:36 -0500

    Updates

---
+++ precache-manifest.36779d65d1bf82a63752a0faf0fd4d53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "133f55e1abda07c9b42e",
+    "url": "/static/js/main.133f55e1.chunk.js"
+  },
+  {
+    "revision": "10296ce2ed0d47d92b2f",
+    "url": "/static/js/1.10296ce2.chunk.js"
+  },
+  {
+    "revision": "133f55e1abda07c9b42e",
+    "url": "/static/css/main.3bff5841.chunk.css"
+  },
+  {
+    "revision": "9c7a40d2ecd7347db3b77ce41cf600e0",
+    "url": "/index.html"
+  }
+];
